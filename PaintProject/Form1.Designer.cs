80ae060cd2b18dd6c21c3ae2763d9681ba42caf1
ibra0022 <82455736+ibra0022@users.noreply.github.com>
2021-05-01 00:28:11 +0300

    Feat: add select ,move and resize

--- PaintProject/Form1.Designer.cs
+++ PaintProject/Form1.Designer.cs
@@ -24,42 +24,43 @@ namespace PaintProject
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.LineButton = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button7 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numericUpDown1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // LineButton
             // 
-            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.button1.Location = new System.Drawing.Point(32, 36);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "خط";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.LineButton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.LineButton.Location = new System.Drawing.Point(28, 29);
+            this.LineButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(82, 23);
+            this.LineButton.TabIndex = 0;
+            this.LineButton.Text = "خط";
+            this.LineButton.UseVisualStyleBackColor = false;
+            this.LineButton.Click += new System.EventHandler(this.LineButton_Click);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.button2.Location = new System.Drawing.Point(32, 88);
+            this.button2.Location = new System.Drawing.Point(28, 70);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.Size = new System.Drawing.Size(82, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "مستطيل";
             this.button2.UseVisualStyleBackColor = false;
@@ -68,9 +69,10 @@ namespace PaintProject
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.button3.Location = new System.Drawing.Point(32, 137);
+            this.button3.Location = new System.Drawing.Point(28, 110);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.Size = new System.Drawing.Size(82, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "دائرة";
             this.button3.UseVisualStyleBackColor = false;
@@ -79,33 +81,31 @@ namespace PaintProject
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.Black;
-            this.button4.Location = new System.Drawing.Point(32, 237);
+            this.button4.Location = new System.Drawing.Point(28, 190);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.Size = new System.Drawing.Size(82, 23);
             this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(32, 181);
+            this.numericUpDown1.Location = new System.Drawing.Point(28, 145);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(94, 27);
+            this.numericUpDown1.Size = new System.Drawing.Size(82, 24);
             this.numericUpDown1.TabIndex = 4;
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Solid",
-            "Dot",
-            "Dash",
-            "Dash dot",
-            "Dash dot dot"});
-            this.comboBox1.Location = new System.Drawing.Point(32, 307);
+            this.comboBox1.Items.AddRange(new object[] {"Solid", "Dot", "Dash", "Dash dot", "Dash dot dot"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 246);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(94, 28);
+            this.comboBox1.Size = new System.Drawing.Size(83, 24);
             this.comboBox1.TabIndex = 5;
             this.comboBox1.Text = "Solid";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
@@ -113,39 +113,31 @@ namespace PaintProject
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.LightGray;
-            this.panel1.Controls.Add(this.button7);
             this.panel1.Controls.Add(this.button6);
             this.panel1.Controls.Add(this.button5);
-            this.panel1.Location = new System.Drawing.Point(-11, -14);
+            this.panel1.Location = new System.Drawing.Point(-10, -11);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(163, 516);
+            this.panel1.Size = new System.Drawing.Size(143, 413);
             this.panel1.TabIndex = 6;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(43, 372);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(94, 29);
-            this.button7.TabIndex = 9;
-            this.button7.Text = "حفظ";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(43, 407);
+            this.button6.Location = new System.Drawing.Point(39, 314);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(94, 29);
+            this.button6.Size = new System.Drawing.Size(82, 23);
             this.button6.TabIndex = 8;
-            this.button6.Text = "استرجاع";
+            this.button6.Text = "تعديل";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(43, 442);
+            this.button5.Location = new System.Drawing.Point(38, 354);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.Size = new System.Drawing.Size(82, 23);
             this.button5.TabIndex = 7;
             this.button5.Text = "جديد";
             this.button5.UseVisualStyleBackColor = true;
@@ -153,18 +145,19 @@ namespace PaintProject
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1231, 496);
+            this.ClientSize = new System.Drawing.Size(1077, 397);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LineButton);
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -173,14 +166,16 @@ namespace PaintProject
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.numericUpDown1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-
         }
 
+        private System.Windows.Forms.Button LineButton;
+
+        private System.Windows.Forms.Button button6;
+
         #endregion
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
@@ -188,8 +183,6 @@ namespace PaintProject
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
     }
 }
 
--- PaintProject/Shape.cs
+++ PaintProject/Shape.cs
@@ -1,62 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Text;
 
 namespace PaintProject
 {
     public abstract class Shape
     {
-        public Point start { get; set; }
-        public Point end { get; set; }
+        public int startX;
+        public int startY;
+        public int endX;
+        public int endY;
+        
         public Color Color { get; set; }
         public Pen Pen { get; set; }
-        public int Hight { get { return Math.Abs(end.Y - start.Y); } set { } } 
-        public int Width { get { return Math.Abs(end.X - start.X); } set { } } 
-        public bool IsSelected { get; set; }
-        public bool Middle { get; set; }
-        public bool Right { get; set; }
-        public bool Top { get; set; }
-        public bool Bottom { get; set; }
-        public bool Left { get; set; }
 
-        //public abstract void draw(Graphics g);
+        public GraphicsPath Path;
+
         public int getWidth()
         {
-            return Math.Abs(end.X - start.X);
+            return Math.Abs(endX - startX);
         }
         public int getHight()
         {
-            return Math.Abs(end.Y - start.Y);
+            return Math.Abs(endY - startY);
         }
         public int getX()
         {
-            if (start.X > end.X)
+            if (startX > endX)
             {
-                return end.X;
+                return endX;
             }
-            return start.X;
+            return startX;
         }
         public int getY()
         {
-            if (start.Y > end.Y)
+            if (startY > endY)
             {
-                return end.Y;
+                return endY;
             }
-            return start.Y;
+            return startY;
         }
     }
 
-    public class Rectangle : Shape
-    {
-
-    }
-    public class Circle : Shape
-    {
-
-    }
-    public class Line : Shape
-    {
-
-    }
+    public class Rectangle : Shape{}
+    public class Circle : Shape{}
+    public class Line : Shape{}
 }
